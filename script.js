1a07d4d4f819467a0ea05511719063a6670f23c6
SrdjanLakic <srdjan949@gmail.com>
2022-04-18 17:50:30 -0400

    Sticky navigation implemented

--- script.js
+++ script.js
@@ -85,3 +85,11 @@ const handleMouseHover = function (e) {
 nav.addEventListener('mouseover', handleMouseHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleMouseHover.bind(1));
+
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+window.addEventListener('scroll', function () {
+  console.log(window.scrollY);
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
